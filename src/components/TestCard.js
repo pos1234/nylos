2a1b6deec0b1fec324d591566dbc5b0b16040b08
pos1234 <paulosegebeyehu5@gmail.com>
2023-07-09 19:44:05 +0700

    fixing performance issues

--- src/components/TestCard.js
+++ src/components/TestCard.js
@@ -3,7 +3,7 @@ const TestCard = (props)=>{
         <div className='font-lexend bg-[#F4F4F4] rounded-xl px-3 py-3 md:px-2 md:py-3 xl:pl-7 xl:pr-10 xl:py-10 '>
             <div className='grid grid-cols-5'>
                 <div className=''>
-                    <img src={props.avatarImage}/>
+                    <img alt="avatar image" src={props.avatarImage}/>
                 </div>
                 <div className='col-span-4 md:pl-3 xl:pl-0'>
                     <p className='font-normal text-base'>{props.name}</p>
@@ -14,7 +14,7 @@ const TestCard = (props)=>{
                 <p className='font-light text-base my-5 md:my-auto'>{props.description}</p>
             </div>
             <div>
-                <img src={props.companyImage}/>
+                <img alt="company image" src={props.companyImage}/>
             </div>
         </div>
     )
